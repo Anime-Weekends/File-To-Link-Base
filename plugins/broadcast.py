87cd8316a9acccb7049fd4cfeab7f5e043a7fe80
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-04-01 15:41:30 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -37,7 +37,7 @@ async def broadcast_messages(user_id, message):
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    sts = await message.reply_text(text='**Broadcasting your messages...**')
+    sts = await message.reply_text(text='**<blockquote>Bʀᴏᴀᴅᴄᴀsᴛɪɴɢ ʏᴏᴜʀ ᴍᴇssᴀɢᴇs...</blockquote>**')
     start_time = time.time()
     total_users = await db.total_users_count()
     done = 0
