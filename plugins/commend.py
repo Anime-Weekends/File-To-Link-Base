b09ef355077dbb6cbe8787e5c812ee34e84974e5
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-04-03 18:22:10 +0530

    Update commend.py

--- plugins/commend.py
+++ plugins/commend.py
@@ -185,8 +185,7 @@ async def about(client, message):
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     me2 = (await client.get_me()).mention
-    await message.reply_photo(
-        photo=random.choice(PICS),
+    await message.reply_text(
         text=script.ABOUT_TXT.format(me2, me2, get_readable_time(time.time() - StartTime), __version__),
         disable_web_page_preview=True, 
         reply_markup=reply_markup
