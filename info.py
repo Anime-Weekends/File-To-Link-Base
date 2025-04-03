9894ebc33b083b6ef90ff0d6774db05deb2cdcf1
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-04-03 17:18:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 OWNER_USERNAME = environ.get("OWNER_USERNAME", 'RexySama') # without @ 
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (environ.get("PICS", "https://i.ibb.co/hxqp9gQk/photo-2025-04-03-11-00-42-7489040182323183640.jpg")).split() #Required
+PICS = (environ.get("PICS", "https://i.ibb.co/hxqp9gQk/photo-2025-04-03-11-00-42-7489040182323183640.jpg https://i.ibb.co/rK42qL4w/photo-2025-04-03-11-48-19-7489052453044748316.jpg")).split() #Required
 
 # channel link information
 CHANNEL = environ.get('CHANNEL', 'https://t.me/EmitingStars_Botz')
