45de7d3d36423541aaaf097d3fa04c17c59765bc
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-04-06 02:02:14 +0530

    Update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -6,7 +6,7 @@ async def showid(client, message):
 
     if chat_type == enums.ChatType.PRIVATE:
         user_id = message.chat.id
-        photo_url = "https://i.ibb.co/q3tWtLCv/photo-2025-04-05-08-59-25-7489751171209363460.jpg"  # Replace with your image URL or file_id
+        photo_url = "https://i.ibb.co/q3twLtCw/photo-2025-04-05-08-59-25-7489751171209363460.jpg"
 
         buttons = InlineKeyboardMarkup([
             [
@@ -17,7 +17,7 @@ async def showid(client, message):
 
         await message.reply_photo(
             photo=photo_url,
-            caption=f"<b>ʏᴏᴜʀ ᴜsᴇʀ ɪᴅ ɪs:</b> <code>{user_id}</code>",
+            caption=f"<b><blockquote>Your User ID is:</b> <code>{user_id}</blockquote></code>",
             reply_markup=buttons,
             quote=True
         )
