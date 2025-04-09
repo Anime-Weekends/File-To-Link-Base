7b37462cb122ea383898ffa032cac34d10b3e4cd
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-10 03:01:40 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -3,28 +3,25 @@ import os
 import time
 from database.users_db import db
 from web.utils.file_properties import get_hash
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 from info import URL, BOT_USERNAME, BIN_CHANNEL, BAN_ALERT, FSUB, CHANNEL
 from utils import get_size
-from Script import script
+from Script import script, IMAGE_URL
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from plugins.avbot import is_user_joined, is_user_allowed
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
-    
+
 @Client.on_message((filters.private) & (filters.document | filters.video | filters.audio), group=4)
 async def private_receive_handler(c: Client, m: Message):
     if FSUB:
         if not await is_user_joined(c, m):
             return
-                
+
     ban_chk = await db.is_banned(int(m.from_user.id))
-    if ban_chk == True:
+    if ban_chk is True:
         return await m.reply(BAN_ALERT)
-    
+
     user_id = m.from_user.id
 
     # ✅ Check if User is Allowed (Limit System)
@@ -42,54 +39,51 @@ async def private_receive_handler(c: Client, m: Message):
 
     try:
         msg = await m.forward(chat_id=BIN_CHANNEL)
-        
+
         stream = f"{URL}watch/{msg.id}?hash={get_hash(msg)}"
         download = f"{URL}{msg.id}?hash={get_hash(msg)}"
         file_link = f"https://t.me/{BOT_USERNAME}?start=file_{msg.id}"
         share_link = f"https://t.me/share/url?url={file_link}"
-        
+
         await msg.reply_text(
             text=f"Requested By: [{m.from_user.first_name}](tg://user?id={m.from_user.id})\nUser ID: {m.from_user.id}\nStream Link: {stream}",
-            disable_web_page_preview=True, quote=True
+            disable_web_page_preview=True,
+            quote=True
         )
 
-# ✅ अगर file_name मौजूद है तो पूरा कैप्शन भेजें, वरना सिर्फ डाउनलोड लिंक भेजें
         if file_name:
-            await m.reply_text(
-                text=script.CAPTION_TXT.format(CHANNEL, file_name, file_size, stream, download),
+            await m.reply_photo(
+                photo=IMAGE_URL,
+                caption=script.CAPTION_TXT.format(CHANNEL, file_name, file_size, stream, download),
                 quote=True,
-                disable_web_page_preview=True,
+                parse_mode="markdown",
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton("Sᴛʀᴇᴀᴍ", url=stream),
                      InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ", url=download)],
-                    [InlineKeyboardButton('Gᴇᴛ ғɪʟᴇ', url=file_link),
-                    InlineKeyboardButton('Sʜᴀʀᴇ', url=share_link),
-                    InlineKeyboardButton('Cʟᴏsᴇ', callback_data='close_data')]
+                    [InlineKeyboardButton("Gᴇᴛ ғɪʟᴇ", url=file_link),
+                     InlineKeyboardButton("Sʜᴀʀᴇ", url=share_link),
+                     InlineKeyboardButton("Cʟᴏsᴇ", callback_data="close_data")]
                 ])
             )
         else:
-            await m.reply_text(
-                text=script.CAPTION2_TXT.format(CHANNEL, file_name, file_size, download),
+            await m.reply_photo(
+                photo=IMAGE_URL,
+                caption=script.CAPTION2_TXT.format(CHANNEL, file_name, file_size, download),
                 quote=True,
-                disable_web_page_preview=True,
+                parse_mode="markdown",
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton("Dᴏᴡɴʟᴏᴀᴅ", url=download),
-                    InlineKeyboardButton('Gᴇᴛ ғɪʟᴇ', url=file_link)],
-                   [ InlineKeyboardButton('Sʜᴀʀᴇ', url=share_link),
-                    InlineKeyboardButton('Cʟᴏsᴇ', callback_data='close_data')]
+                     InlineKeyboardButton("Gᴇᴛ ғɪʟᴇ", url=file_link)],
+                    [InlineKeyboardButton("Sʜᴀʀᴇ", url=share_link),
+                     InlineKeyboardButton("Cʟᴏsᴇ", callback_data="close_data")]
                 ])
-             )
+            )
 
     except FloodWait as e:
-        print(f"Sleeping for {e.value}s")
+        print(f"Sleeping for {e.value}s due to FloodWait")
         await asyncio.sleep(e.value)
         await c.send_message(
             chat_id=BIN_CHANNEL,
             text=f"Gᴏᴛ FʟᴏᴏᴅWᴀɪᴛ ᴏғ {e.value}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**𝚄𝚜𝚎𝚛 𝙸𝙳 :** `{m.from_user.id}`",
             disable_web_page_preview=True
-           )
-
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
-    
+        )
