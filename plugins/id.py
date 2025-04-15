e35325940b74234d1b766712a1ef346c31cb34e3
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-16 01:08:45 +0530

    Update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -2,21 +2,21 @@ from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot  # This now works!
 
-@Bot.on_message(filters.command("id") & filters.private)
+@Bot.on_message(filters.command("myid") & filters.private)
 async def showid(client, message):
     user_id = message.chat.id
-    photo_url = "https://i.ibb.co/d0Xg3xMB/photo-2025-04-06-10-07-03-7490139616641548308.jpg"
+    photo_url = "https://i.ibb.co/YzFqHky/photo-2025-04-15-09-14-30-7493465832589099024.jpg"
 
     buttons = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("Button 1", url=f"https://t.me/RexySama"),
-            InlineKeyboardButton("Button 2", url=f"https://t.me/RexySama")
+            InlineKeyboardButton("Oᴡɴᴇʀ", url=f"https://t.me/RexySama"),
+            InlineKeyboardButton("Dᴇᴠᴇʟᴏᴘᴇʀ", url=f"https://t.me/RexySama")
         ]
     ])
 
     await message.reply_photo(
         photo=photo_url,
-        caption=f"<b><blockquote>Your User ID is:</b> <code>{user_id}</blockquote></code>",
+        caption=f"<b><blockquote>Yᴏᴜʀ ᴜsᴇʀ ɪᴅ ɪs:</b> <code>{user_id}</blockquote></code>",
         reply_markup=buttons,
         quote=True
     )
