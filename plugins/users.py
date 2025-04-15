599243a7468c438fa278682c3298d6e8fedcb070
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-16 01:12:05 +0530

    Update users.py

--- plugins/users.py
+++ plugins/users.py
@@ -6,9 +6,6 @@ from database.users_db import db
 from pyrogram import Client, filters
 from info import ADMINS
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
 
 @Client.on_message(filters.private & filters.command("users") & filters.user(ADMINS))
 async def users(bot, update):
@@ -21,9 +18,6 @@ async def users(bot, update):
         disable_web_page_preview=True
     )
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
 
 @Client.on_message(filters.private & filters.command(['restart']) & filters.user(ADMINS))
 async def restart(client, message):
@@ -34,7 +28,3 @@ async def restart(client, message):
     await asyncio.sleep(2)
     await msg.edit("<i>Server Restarted Successfully ✅</i>")
     os.execl(sys.executable, sys.executable, *sys.argv)
-
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
